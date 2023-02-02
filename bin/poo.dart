eda166b5fdff0f13976195975d99d626405a03e6
Nicolas Devel <nicolassabogal85@gmail.com>
2023-02-02 16:24:10 -0500

    start project

---
+++ bin/poo.dart
@@ -0,0 +1,5 @@
+import 'package:poo/poo.dart' as poo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${poo.calculate()}!');
+}
---
+++ test/poo_test.dart
@@ -0,0 +1,8 @@
+import 'package:poo/poo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
