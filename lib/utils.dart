dac49b03cadbdfb4f4732f939a1fd7cc08739fa7
Johan Estiben Rincon Zambrano <johan.rincon@ghtcorp.com>
2023-02-07 14:40:16 -0500

    fixed 1

--- lib/utils.dart
+++ lib/utils.dart
@@ -11,11 +11,21 @@ bool isPalindrome(String text){
 }
 
 List<int> burbleShort(List<int> list){
-  return list;
-
-
-  ////
+  
+  int lengthOfArray = list.length; 
+  for (int i = 0; i < lengthOfArray - 1; i++) { 
+    for (int j = 0; j < lengthOfArray - i - 1; j++) { 
+      if (list[j] > list[j + 1]) { 
+  
+        int temp = list[j]; 
+        list[j] = list[j + 1]; 
+        list[j + 1] = temp; 
+      } 
+    } 
+  } 
+  return (list); 
 }
+ 
 
 List<int> quickSort(List<int> list){
   return list;
