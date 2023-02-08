12ec4546bdab6dcb23cfbecfc20bb168b232d406
paola cotrina <luism.taqued@utadeo.edu.co>
2023-02-08 01:01:06 -0500

    ltaque actualizacion archivos 1 algoritmos

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,16 +1,68 @@
 
 List<int> fibonacci(int maxNumber){
-  return [1,2,3,4,5];
+   int var1 = 1;
+  int var2 = 1;
+  int count = 0;
+  int fibo = 0;
+  var fibonacci=[];
+  while (count <= 25) {
+    fibonacci.add(var1);
+    fibo = var1 + var2;
+    var1 = var2;
+    var2 = fibo;
+    count += 1;
+
+  }
+return fibonacci;
 }
 
+
+
+
+
+
 bool isPalindrome(String text){
-  return true;
+String cadena= "A ti no, bonita.";
+   String cadenatratada=cadena.toLowerCase().replaceAll(' ', '').replaceAll(',', '').replaceAll('.', '');
+    bool estado;
+  
+   String cadenaInvertida=""; 
+
+    for (var i = cadenatratada.length - 1; i >= 0; i--) {
+       cadenaInvertida += String.fromCharCode(cadenatratada.runes.elementAt(i));
+    } 
+   if (cadenatratada==cadenaInvertida){
+     estado=true;
+    } 
+   else{
+      estado=false;
+   }
+  return estado;
 }
 
 List<int> burbleShort(List<int> list){
-  return list;
+ var lista=[2,12,52,62,1,0,-10,-750];
+  var longitud = lista.length;
+  for (var i = 0; i < longitud; i++) { 
+    for (var j = 0; j < longitud - i - 1; j++) {
+      if (lista[j] > lista[j + 1]) {
+        var temp = lista[j];
+        lista[j] = lista[j + 1];
+        lista[j + 1] = temp;
+      }
+    }
+  }
+return lista;
+
+
+
 }
 
 List<int> quickSort(List<int> list){
-  return list;
+    Map<String, int> vectores = {'a': 1, 'b': 2};
+
+  List<int> listadatos = [-9999999999,-12,-000000000,-1,-2,99,678,88,10,15,14,];
+
+  listadatos.sort((int a, int b) => a.compareTo(b));
+  return listadatos;
 }
