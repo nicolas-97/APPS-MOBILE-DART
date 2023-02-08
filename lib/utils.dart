27ed7a6f77d3ff78fceda3a04aee6d302b9d6062
ltaqueutadeo <124324406+ltaqueutadeo@users.noreply.github.com>
2023-02-07 21:31:02 -0500

    Update utils.dart

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,21 @@
 
 List<int> fibonacci(int maxNumber){
-  return [1,2,3,4,5];
+ print('**-------Serie Fibonacci------**');
+  int var1 = 1;
+  int var2 = 1;
+  int count = 0;
+  int fibo = 0;
+
+var arreglo =[]  ;
+  while (count <= maxNumber) {
+    print(var1);
+    arreglo.add(var1);
+    fibo = var1 + var2;
+    var1 = var2;
+    var2 = fibo;
+    count += 1;
+  }
+  return arreglo;
 }
 
 bool isPalindrome(String text){
